71ba0b2247d7a8365635fc9fd88648675a2bc279
rahimabadshop <94161226+rahimabadshop@users.noreply.github.com>
2021-11-14 08:47:53 +0530

    site

---
+++ Submit.php
@@ -0,0 +1,68 @@
+<?php
+//sample php code
+
+//this will collect data from form
+$provider_id = $_POST['provider_id']; 
+$number = $_POST['number'];
+$amount = $_POST['amount'];
+$client_id = "09919190"; //(your system unique id. that you can check in pay2all);
+//end of data collection from form
+
+
+//check whether user enter some data or not
+        if(empty($provider_id)){
+        echo"select operator";
+        exit;
+        }
+        if(empty($number)){
+        echo"enter mobile number";
+        exit;
+        }
+        if(empty($amount)){
+        echo"enter amount";
+        exit;
+        }
+
+
+        $parameters = array(
+                'number' => $number,
+                'provider_id' => $provider_id // Provider id check in pay2all
+                'amount' => $amount, // Recharge or payment Amount
+                'client_id' => $client_id // your system unique id
+                'optional1' => '',
+                'optional2' => '',
+                'optional3' => '',
+                'optional4' => ''
+        );
+
+        $access_token = ""; //you have to add personal access token 
+
+    
+
+        $header = ["Accept:application/json", "Authorization:Bearer ".$access_token];
+
+        $method = 'POST';
+
+        $url = 'https://pay2all.in/api/v1/transaction';
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLINFO_HEADER_OUT, 1);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($parameters));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        $response = curl_exec($ch);
+        echo $response;  //[JSON RESPONSE]
+
+
+     
+
+
+?>
